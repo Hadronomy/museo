618fb2303585fb60801d3ac63c6e51b1e85380d0
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-03-22 00:27:25 +0000

    feat: add test scene

---
+++ src/components/Test.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Canvas } from '@threlte/core';
+  import Scene from './Scene.svelte';
+  import { T } from '@threlte/core';
+  import { XR, Controller, ARButton } from '@threlte/xr';
+  import { OrbitControls } from '@threlte/extras';
+</script>
+
+<div class="min-h-screen w-full">
+  <Canvas>
+    <Scene />
+
+    <XR>
+      <Controller left />
+      <Controller right />
+      <svelte:fragment slot="fallback">
+        <T.PerspectiveCamera makeDefault position.z={5}>
+          <OrbitControls />
+        </T.PerspectiveCamera>
+      </svelte:fragment>
+    </XR>
+  </Canvas>
+  <ARButton />
+</div>
