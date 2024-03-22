2c37278b6d5834ce56fb3bf01b7569028db724d9
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-03-22 01:25:45 +0000

    fix: move scene out of xr

--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -8,17 +8,12 @@
 
 <div class="relative min-h-dvh w-full">
   <Canvas>
+    <Scene />
     <XR>
       <Controller left />
       <Controller right />
 
-      <T.Mesh>
-        <T.BoxGeometry attach="geometry" />
-        <T.MeshBasicMaterial attach="material" />
-      </T.Mesh>
-
       <svelte:fragment slot="fallback">
-        <Scene />
         <T.PerspectiveCamera makeDefault position.z={5}>
           <OrbitControls />
         </T.PerspectiveCamera>
